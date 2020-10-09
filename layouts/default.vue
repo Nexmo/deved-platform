026a48f9dd1fe566ac50b792812b3a6d09c0c653
SkyWolf369 <Captainawesome829@gmail.com>
2020-10-09 11:38:45 -0400

    Added Notice in Console

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,13 +10,15 @@
 import Footer from "~/components/Footer"
 import Header from "~/components/Header"
 
+
 export default {
   components: {
     Footer,
     Header,
   },
-  
+
   head() {
+  console.log("YOU SHOULDNT BE HERE... But as you are we're hiring folks who know there way around a console find a career at https://developer.nexmo.com/careers");
     return {
       script: [
         { src: "https://identity.netlify.com/v1/netlify-identity-widget.js", defer: true },
@@ -25,3 +27,4 @@ export default {
   },
 }
 </script>
+
